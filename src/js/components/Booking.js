8519e64464bb936e305bbe99d702574c285caa05
Agnieszka Ciesińska <agnieszkaciesinska@gmail.com>
2023-07-11 19:46:30 +0200

    Error HourPicker

--- src/js/components/Booking.js
+++ src/js/components/Booking.js
@@ -1,5 +1,7 @@
 import { templates, select, settings, classNames } from '../settings.js';
 import AmountWidget from './AmountWidget.js';
+import DatePicker from './DatePicker.js';
+import HourPicker from './HourPicker.js';
 import utils from '../utils.js';
 
 class Booking {
@@ -25,6 +27,13 @@ class Booking {
           thisBooking.dom.hoursAmount = thisBooking.dom.wrapper.querySelector(
             select.booking.hoursAmount
           );
+
+          thisBooking.dom.datePicker = thisBooking.dom.wrapper.querySelector(
+            select.widgets.datePicker.wrapper
+          );
+          thisBooking.dom.hourPicker = thisBooking.dom.wrapper.querySelector(
+            select.widgets.hourPicker.wrapper
+          );
     }
 
     initWidgets() {
@@ -35,6 +44,12 @@ class Booking {
     
         thisBooking.hoursAmount = new AmountWidget(thisBooking.dom.hoursAmount);
         thisBooking.dom.hoursAmount.addEventListener('updated', function () {});
+
+        thisBooking.datePicker = new DatePicker(thisBooking.dom.datePicker);
+        thisBooking.dom.datePicker.addEventListener('updated', function () {});
+    
+        thisBooking.hourPicker = new HourPicker(thisBooking.dom.hourPicker);
+        thisBooking.dom.hourPicker.addEventListener('updated', function () {});
     }
 }
 
